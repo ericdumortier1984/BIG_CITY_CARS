5cceff8a710502016f1f7c592f9afe0e2f100c77
ericdumortier1984 <145196569+ericdumortier1984@users.noreply.github.com>
2025-04-02 09:38:43 -0300

    Update Scene
    Add more Cars

--- MO3D_Final/Assets/Scripts/Scene0/CarSelectionController.cs
+++ MO3D_Final/Assets/Scripts/Scene0/CarSelectionController.cs
@@ -1,13 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CarSelectionController : MonoBehaviour
 {
+	[Header("Car Selection")]
     public GameObject[] mCarsToSelect; // Array de autos seleccionables
     public int mSelectedCarIndex = 0; // Indice de autos seleccionados
 
-    public void NextCarSelection() // Metodo para seleccionar siguiente auto del array
+	[Header("Selected Car Stats")]
+	public string mSelectedCarName; // Nombre del auto seleccionado
+	public string mSelectedCarPrice; // Precio del auto seleccionado
+	public float mSelectedCarSpeed; // Velocidad del auto seleccionado
+	public float mSelectedCarAcceleration; // Aceleratcion del auto seleccionado
+	public float mSelectedCarBreaking; // Frenado del auto seleccionado
+	public float mSelectedCarHandling; // Manejo del auto seleccionado
+
+	[Header("UI Elements")]
+	public TMPro.TextMeshProUGUI mCarNameText; // Referencia al texto del nombre del auto
+	public TMPro.TextMeshProUGUI mCarPriceText; // Referencia al texto del precio del auto
+	public Slider mCarSpeedSlider; // Referencia al slider de velocidad
+	public Slider mCarAccelerationSlider; // Referencia al slider de aceleración
+	public Slider mCarBreakingSlider; // Referencia al slider de frenado
+	public Slider mCarHandlingSlider; // Referencia al slider de manejo
+
+	private void Start()
+	{
+		UpdateCarStats();
+	}
+
+	public void NextCarSelection() // Metodo para seleccionar siguiente auto del array
     {
         mCarsToSelect[mSelectedCarIndex].SetActive(false); // seteamos falso al principio
         mSelectedCarIndex = (mSelectedCarIndex + 1) % mCarsToSelect.Length; // Calculamos por la cantidad de autos 
@@ -18,6 +42,7 @@ public class CarSelectionController : MonoBehaviour
 		PlayerPrefs.SetInt("mSelectedCarIndex", mSelectedCarIndex);
 		PlayerPrefs.Save();
 
+		UpdateCarStats();
 	}
 
 	public void PreviousCarSelection() // Metodo para seleccionar el anterior auto del array
@@ -34,5 +59,27 @@ public class CarSelectionController : MonoBehaviour
 		// Guardamos el índice seleccionado
 		PlayerPrefs.SetInt("mSelectedCarIndex", mSelectedCarIndex);
 		PlayerPrefs.Save();
+
+		UpdateCarStats();
+	}
+
+	private void UpdateCarStats()
+	{
+		CarStats mCarStats = mCarsToSelect[mSelectedCarIndex].GetComponent<CarStats>(); // Referencia al script de las estadisticas de los autos
+
+		mSelectedCarName = mCarStats.CarName;
+		mSelectedCarPrice = mCarStats.CarPrice;
+		mSelectedCarSpeed = mCarStats.CarSpeed;
+		mSelectedCarAcceleration = mCarStats.CarAcceleration;
+		mSelectedCarBreaking = mCarStats.CarBreaking;
+		mSelectedCarHandling = mCarStats.CarHandling;
+
+		// Actualizar UI de estadisticas dela uto seleccionado
+		mCarNameText.text = mSelectedCarName;
+		mCarPriceText.text = mSelectedCarPrice;
+		mCarSpeedSlider.value = mSelectedCarSpeed;
+		mCarAccelerationSlider.value = mSelectedCarAcceleration;
+		mCarBreakingSlider.value = mSelectedCarBreaking;
+		mCarHandlingSlider.value = mSelectedCarHandling;
 	}
 }
